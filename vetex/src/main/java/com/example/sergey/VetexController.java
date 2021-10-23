7fafa483b40884a6435edd357b34f440e49cdfed
spavlov <spavlov@0300NBA01141TBN.nw.mts.ru>
2021-10-23 20:30:36 +0300

    reject pdforder

--- vetex/src/main/java/com/example/sergey/VetexController.java
+++ vetex/src/main/java/com/example/sergey/VetexController.java
@@ -1,6 +1,5 @@
 package com.example.sergey;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -8,20 +7,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-
-import com.itextpdf.html2pdf.HtmlConverter;
 
 @Controller
 public class VetexController {
@@ -122,7 +112,7 @@ public class VetexController {
 		return "redirect:/priceItems";
 	}
 	
-	@GetMapping ("/showCart")
+	@GetMapping ("/dispOrder")
 	public String showCart(Model model) {
 		sumWithOutNds=0;
 		Nds=0;
@@ -147,66 +137,32 @@ public class VetexController {
 		model.addAttribute("sumWithOutNds", this.sumWithOutNds);
 		model.addAttribute("Nds", this.Nds);
 		model.addAttribute("sumWithNds", this.sumWithNds);
-		return"showCart";
+		return"dispOrder";
 	}
 	@GetMapping ("/clearCart")
 	public String clearCart() {
 		orderCart.clearCart();
-		return "redirect:/showCart";
+		return "redirect:/dispOrder";
 	}
 	@GetMapping ("/clearCart2")
 	public String clearCart2() {
 		orderCart.clearCart();
 		return "redirect:/priceItems";
 	}
-	@GetMapping ("/createOrder")
-	public ResponseEntity<byte[]> getPDF() throws IOException {
-
-        /* Create HTML using Thymeleaf template Engine */
-		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode(TemplateMode.HTML);
-        templateResolver.setCacheable(false);
-        TemplateEngine templateEngine = new TemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver);
-        
-        Context context = new Context();
-        context.setVariable("cart", cart);
-        context.setVariable("sumWithOutNds", sumWithOutNds);
-        context.setVariable("Nds", Nds);
-        context.setVariable("sumWithNds", sumWithNds);
-        String orderHtml = templateEngine.process("showCart", context);
-
-        /* Setup Source and target I/O streams */
-        String filename="order.pdf";
-        ByteArrayOutputStream target = new ByteArrayOutputStream();
-        
-        /* Call convert method */
-        HtmlConverter.convertToPdf(orderHtml, target);
-
-        /* extract output as bytes */
-        byte[] bytes = target.toByteArray();
-
-        /* Send the response as downloadable PDF */
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename="+ filename)
-                .contentType(MediaType.APPLICATION_PDF)
-                .body(bytes);
-
-    }
+	
 	@GetMapping ("/deleteFromOrder")
 	public String deleteFromOrder(@RequestParam("ppnumber")String ppnumber,@RequestParam("quantity")double quantity) {
 		orderCart.deleteItem(ppnumber,quantity);
-		return "redirect:/showCart";
+		return "redirect:/dispOrder";
 	}
 	@GetMapping("/403")
     public String error403() {
-        return "/error/403";
+        return "403";
     }
 	@GetMapping ("/saveQuantityChanges")
 	public String saveQuantityChanges(@RequestParam("ppnumber")String ppnumber,@RequestParam("quantity")double quantity,@RequestParam("newQuantity")double newQuantity) {
 		orderCart.saveQuantityItem(ppnumber,quantity,newQuantity);
-		return "redirect:/showCart";
+		return "redirect:/dispOrder";
 	}
 	@GetMapping ("/orderPage")
 	public String tableOrder(Model model) {
