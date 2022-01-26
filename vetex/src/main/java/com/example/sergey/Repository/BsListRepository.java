b95c71f52b4c9f7aafccb7cdc038e52a83505aeb
spavlov <spavlov@host.docker.internal>
2022-01-27 00:29:32 +0300

    bslist edit + selected admin

--- vetex/src/main/java/com/example/sergey/Repository/BsListRepository.java
+++ vetex/src/main/java/com/example/sergey/Repository/BsListRepository.java
@@ -1,13 +1,21 @@
 package com.example.sergey.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.example.sergey.Model.BsList;
+import com.example.sergey.Model.Order;
 
 public interface BsListRepository extends JpaRepository<BsList,Long> {
 
+	//поиск БС по её номеру (надо вводить точный номер БС, для заполнения заявки)
 	@Query(value="select * from bslist where bsnumber in(:filter)",nativeQuery=true)
 	public BsList findBsByNumber(@Param("filter") String filter);
+	
+	//поиск БС по её номеру (можно ввести часть номера БС)
+	@Query(value="select * from bslist where lower(bsnumber) like concat('%',:bsnum,'%')",nativeQuery=true)
+	public List<BsList> findBsByNumber2(@Param("bsnum") String bsnum);
 }
