f6c2d895d2ef0e32bbd58f704d295b2db2548844
Sergey Pavlov <s.a.pavlov@mail.ru>
2022-09-10 10:17:49 +0300

    new server for app

--- src/main/java/com/example/sergey/Controller/GeneralController.java
+++ src/main/java/com/example/sergey/Controller/GeneralController.java
@@ -1,45 +1,14 @@
 package com.example.sergey.Controller;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.example.sergey.Model.AfuOrdersCount;
-import com.example.sergey.Service.ContractorService;
-
 @Controller
 public class GeneralController {
-
-	@Autowired private ContractorService contractorService;
 	
-	@GetMapping("/login")
-	public String login() {
-		return "login";
-	}
 	@GetMapping("/") //переход на страницу index (главную) с передачей данных о подрядчиках
 	public String index(Model model) {
-				
-		List<AfuOrdersCount> countSumContractorAfuInfra=contractorService.countSumContractorAfuInfra();
-		
-	    Date contractEndDate = null;
-			    
-	    for(AfuOrdersCount countSumContractorAfuInfraItem:countSumContractorAfuInfra) {
-		SimpleDateFormat formatterStringToDate=new SimpleDateFormat("yyyy-MM-dd");
-		
-		try {contractEndDate=formatterStringToDate.parse(countSumContractorAfuInfraItem.getContractEnd());} catch (ParseException e) {e.printStackTrace();}
-		
-		SimpleDateFormat formatterDateToString=new SimpleDateFormat("dd.MM.yyyy");
-		String contractEndString=formatterDateToString.format(contractEndDate);
-		countSumContractorAfuInfraItem.setContractEnd(contractEndString);
-	}
-		
-		model.addAttribute("countSumContractorAfuInfra", countSumContractorAfuInfra);
 		return "index";
 	}
 	
--- src/main/java/com/example/sergey/MyDbConnection.java
+++
@@ -1,8 +0,0 @@
-package com.example.sergey;
-
-//данные для подключения к БД, класс используется для up-download файлов .csv
-public class MyDbConnection {
-	public static String urls = new String("jdbc:postgresql://ec2-63-32-173-118.eu-west-1.compute.amazonaws.com:5432/d77nv9mhg4ler3");
-	public static String username = new String("fdqklynbbsxglj");
-	public static String password = new String("6270faaec69da2f9a2d3ea45a02a7336d0a1f266dda7401ab4b8bf9c903262ab");
-}
