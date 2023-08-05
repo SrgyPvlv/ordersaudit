4237b8229d689e1b9021342d9d39ee14af8eb277
Sergey Pavlov <s.a.pavlov@mail.ru>
2023-08-05 23:10:05 +0300

    first commit

--- src/main/java/com/example/sergey/MyDbConnection.java
+++ src/main/java/com/example/sergey/MyDbConnection.java
@@ -2,7 +2,7 @@ package com.example.sergey;
 
 //данные для подключения к БД, класс используется для up-download файлов .csv
 public class MyDbConnection {
-	public static String urls = new String("jdbc:postgresql://10.232.3.49:5432/ordersaudit?sslmode=require");
-	public static String username = new String("sergey");
-	public static String password = new String("rbs123");
+	public static String urls = new String("jdbc:postgresql://localhost:5432/ordersaudit");
+	public static String username = new String("postgres");
+	public static String password = new String("180578lord");
 }
